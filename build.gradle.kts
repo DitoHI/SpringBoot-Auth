20cdba2aaa5d6a4bc3feba4f61a5e37cfba51524
ditohafizh <ditohafizh@gmail.com>
2019-05-19 17:21:09 +0700

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.2.0.M3"
+	id("io.spring.dependency-management") version "0.6.0.RELEASE"
+	kotlin("jvm") version "1.3.31"
+	kotlin("plugin.spring") version "1.3.31"
+}
+
+group = "com.diaryquran"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+	jcenter()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven {
+		url = uri("https://plugins.gradle.org/m2/")
+	}
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jlleitschuh.gradle:ktlint-gradle:8.0.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	// additional
+	implementation("org.springframework.cloud:spring-cloud-gcp-starter-config:1.0.0.M3")
+	implementation("com.graphql-java:graphql-java:8.0")
+	implementation("com.graphql-java:graphiql-spring-boot-starter:4.0.0")
+	implementation("com.graphql-java:graphql-spring-boot-starter:4.0.0")
+	implementation("com.graphql-java:graphql-java-tools:5.0.0")
+	implementation("com.graphql-java:graphql-java-servlet:5.0.0")
+	implementation("org.postgresql:postgresql:42.2.5")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.1.5.RELEASE")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
+
---
+++ src/main/kotlin/com/diaryquran/server/model/User.kt
@@ -0,0 +1,40 @@
+package com.diaryquran.server.model
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotNull
+
+@Entity
+@Table(name = "user")
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long = 0,
+
+    @Column(name = "username")
+    @field:NotNull(message = "Username cannot be Null")
+    var username: String,
+
+    @Column(name = "email")
+    @field:NotNull(message = "Email cannot be Null")
+    @field:Email
+    var email: String = "",
+
+    @Column(name = "password")
+    @field:NotNull(message = "Password cannot be Null")
+    var password: String = "",
+
+    @Column(name = "name")
+    var name: String = "",
+
+    @Column(name = "age")
+    var age: String = "",
+
+    @Column(name = "photo")
+    var photo: String = ""
+)
